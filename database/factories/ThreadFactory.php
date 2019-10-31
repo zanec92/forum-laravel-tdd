fe38487c017ba31a77957d2670b8a2555bed770e
developer <developer@bloknot.desktop>
2019-10-31 14:56:24 +0300

    initial database setup with seeding

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Thread;
+use Faker\Generator as Faker;
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => function() {
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
