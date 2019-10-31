b9c0837fefefcf9d628a2c10a04d27347db4ae49
developer <developer@bloknot.desktop>
2019-10-31 16:44:19 +0300

    test driving threads

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $threads = factory('App\Thread', 50)->create();
+
+        $threads->each(function($thread) {
+            factory('App\Reply', 10)->create(['thread_id' => $thread->id]);
+        });
     }
 }
