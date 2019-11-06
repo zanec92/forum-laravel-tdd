cda65cf7376f7372db1ff2046ec5e251cdb51c46
Ваше Имя <you@example.com>
2019-11-07 00:02:19 +0300

    a user can filter threads by username

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         factory('App\User')->create([
+            'name' => 'JohnDoe',
             'email' => 'john@doe.com'
         ]);
 
