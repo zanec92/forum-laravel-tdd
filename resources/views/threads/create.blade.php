f2a5aaf0f19192aa30fd3e84d57132d34a776959
Ваше Имя <you@example.com>
2019-11-06 22:55:16 +0300

    validation errors and old data

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,18 +10,41 @@
                 <div class="panel-body">
                     <form action="/threads" class="panel-body" method="POST">
                         {{ csrf_field() }}
+
+                        .<div class="form-group">
+                            <label for="channel_id">Choose a Channel:</label>
+                            <select name="channel_id" id="channel_id" class="form-control" required>
+                                <option value="">Choose one...</option>
+                                @foreach (App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
                         <div class="form-group">
                             <label for="title">Title: </label>
-                            <input type="text" class="form-control" id="title" name="title">
+                            <input type="text" class="form-control" id="title" name="title" 
+                                value="{{ old('title') }}" required>
                         </div>
 
                         <div class="form-group">
                             <label for="body">Body:</label>
-                            <textarea id="body" class="form-control" name="body" rows="8"></textarea>
+                            <textarea id="body" class="form-control" name="body" 
+                                rows="8" required>{{ old('body') }}</textarea>
                         </div>
 
-                        <button type="submit" class="btn btn-primary">Publish</button>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Publish</button>
+                        </div>
                     </form>
+
+                    @if (count($errors))
+                        <ul class="alert alert-danger">
+                            @foreach($errors->all() as $error)
+                               <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
                 </div>
             </div>
         </div>
