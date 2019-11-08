2db4ed850048054ee5d967cc1b8571954197b616
Ваше Имя <you@example.com>
2019-11-08 20:00:19 +0300

    the favorite button

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,22 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{ $reply->created_at->diffForHumans() }}...
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a> said {{ $reply->created_at->diffForHumans() }}...
+            </h5>
+
+            <div>
+                <form action="/replies/{{ $reply->id }}/favorites" method="POST">
+                    {{ csrf_field() }}
+
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="panel-body">
         {{ $reply->body }}
